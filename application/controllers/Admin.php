e5d88bfe018a3d9e11ad21cb631312ce1ac6ae3c
Suluh Arwani <suluharwani007@gmail.com>
2019-04-27 12:46:37 +0700

    stat

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -3,12 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
 	function __construct() {
-    parent::__construct();
-    $this->load->model('mdl_login');
-    date_default_timezone_set('Asia/Jakarta');
-  }
+		parent::__construct();
+		$this->load->model('mdl_login');
+		date_default_timezone_set('Asia/Jakarta');
+	}
 	public function index()
 	{
+
 		$web_info = $this->web_info();
 
 		$data['title'] = $web_info['nama_web'];
@@ -37,89 +38,97 @@ class Admin extends CI_Controller {
 
 
 	}
-	 function _admin_data(){
+	function _admin_data(){
 		// $this->_make_sure_is_admin();
-         	$admin_data = $this->admin_info();
-         	foreach ($admin_data->result() as $value) {
-         		$adm['id_admin'] = $value->id_admin;
-         		$adm['nama_admin'] = $value->nama;
-         		$adm['level_admin'] = $value->level;
-         		$adm['foto_admin'] = $value->foto;
-         		$adm['alamat_admin'] = $value->alamat;
-         		$adm['nomor_telepon_admin'] = $value->nomor_telepon;
-         		$adm['email_admin'] = $value->email;
-         	}
-         	if ($adm['level_admin'] == 1) {
-         		$adm['jabatan_admin'] = "Super Admin";
-         	}else{
-         		$adm['jabatan_admin'] = "Admin";
-         	}
-         	return $adm;
-         }
-         function web_info(){
-         	$this->load->model('Mdl_web_profile');
-         	$info_web = $this->Mdl_web_profile->info_profile();
-         	foreach ($info_web->result() as $inf) {
-         		if (isset($inf->nama_web)) {
-         			$info['nama_web'] = $inf->nama_web;
-         		}else {
-         			$info['nama_web'] = "";
-         		}
-         		if (isset($inf->alamat)) {
-         			$info['alamat'] = $inf->alamat;
-         		}else {
-         			$info['alamat']="";
-         		}
-         	}
-         	return $info;
-         }
-         function admin_info(){
+		$admin_data = $this->admin_info();
+		foreach ($admin_data->result() as $value) {
+			$adm['id_admin'] = $value->id_admin;
+			$adm['nama_admin'] = $value->nama;
+			$adm['level_admin'] = $value->level;
+			$adm['foto_admin'] = $value->foto;
+			$adm['alamat_admin'] = $value->alamat;
+			$adm['nomor_telepon_admin'] = $value->nomor_telepon;
+			$adm['email_admin'] = $value->email;
+		}
+		if ($adm['level_admin'] == 1) {
+			$adm['jabatan_admin'] = "Super Admin";
+		}else{
+			$adm['jabatan_admin'] = "Admin";
+		}
+		return $adm;
+	}
+	function web_info(){
+		$this->load->model('Mdl_web_profile');
+		$info_web = $this->Mdl_web_profile->info_profile();
+		foreach ($info_web->result() as $inf) {
+			if (isset($inf->nama_web)) {
+				$info['nama_web'] = $inf->nama_web;
+			}else {
+				$info['nama_web'] = "";
+			}
+			if (isset($inf->alamat)) {
+				$info['alamat'] = $inf->alamat;
+			}else {
+				$info['alamat']="";
+			}
+		}
+		return $info;
+	}
+	function admin_info(){
 		// $this->_make_sure_is_admin();
-         	$id_admin = $this->session->userdata('id_admin_login');
-         	if ($id_admin == null) {
-         		redirect('login','refresh');
-         	}else{
-         		$this->db->select('* ,admin.id as id_admin');
-         		$this->db->from('admin');
-         		$this->db->join('contact_admin', 'admin.id = contact_admin.id_admin', 'left');
-         		$this->db->join('data_admin', 'admin.id = data_admin.id_admin', 'left');
-         		$this->db->where('admin.id', $id_admin);
-         		$admin = $this->db->get();
-         	}
-         	return $admin;
-         }
-         function _make_sure_is_super_admin(){
-         	$id_admin = $this->session->userdata('id_admin_login');
-         	$admin = $this->db->get_where('admin', array('id'=>$id_admin));
-         	foreach ($admin->result() as $value) {
-         		$level = $value->level;
-         	}
-         	if (isset($level)) {
-         		if ($level != "1") {
-         			$this->session->sess_destroy();
-         			redirect('login','refresh');
-         		}
-         	}else{
-         		$this->session->sess_destroy();
-         		redirect('login','refresh');
-         	}
-         }
+		$id_admin = $this->session->userdata('id_admin_login');
+		if ($id_admin == null) {
+			redirect('login','refresh');
+		}else{
+			$this->db->select('* ,admin.id as id_admin');
+			$this->db->from('admin');
+			$this->db->join('contact_admin', 'admin.id = contact_admin.id_admin', 'left');
+			$this->db->join('data_admin', 'admin.id = data_admin.id_admin', 'left');
+			$this->db->where('admin.id', $id_admin);
+			$admin = $this->db->get();
+		}
+		return $admin;
+	}
+	function _make_sure_is_super_admin(){
+		$id_admin = $this->session->userdata('id_admin_login');
+		$admin = $this->db->get_where('admin', array('id'=>$id_admin));
+		foreach ($admin->result() as $value) {
+			$level = $value->level;
+		}
+		if (isset($level)) {
+			if ($level != "1") {
+				$this->session->sess_destroy();
+				redirect('login','refresh');
+			}
+		}else{
+			$this->session->sess_destroy();
+			redirect('login','refresh');
+		}
+	}
 	function _make_sure_is_admin(){
-         	$is_user = $this->session->userdata('status_login_admin');
-         	$admin_data = $this->admin_info();
-         	foreach ($admin_data->result() as $value) {
-         		$level_admin = $value->level;
-         	}
+		$is_user = $this->session->userdata('status_login_admin');
+		$admin_data = $this->admin_info();
+		foreach ($admin_data->result() as $value) {
+			$level_admin = $value->level;
+		}
 
-         	if ($is_user == "admin_login") {
-         		if ($level_admin==3) {
-         			redirect("/dashboard",'refresh');
-         		}
-         	}else {
-         		$this->session->sess_destroy();
-         		redirect('login','refresh');
-         	}
-         }
+		if ($is_user == "admin_login") {
+			if ($level_admin==3) {
+				redirect("/dashboard",'refresh');
+			}
+		}else {
+			$this->session->sess_destroy();
+			redirect('login','refresh');
+		}
+	}
+function convert($size)
+{
+    $unit=array('b','kb','mb','gb','tb','pb');
+    return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
+}
+function memory_usage(){
+	echo $this->convert(memory_get_usage(true)); // 123 kb
+}
 
 
 
