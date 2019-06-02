65a183ebe31f6c7bb84fad5f4303ae96471780fc
Suluh Arwani <suluharwani007@gmail.com>
2019-06-02 20:47:31 +0700

    database

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -47,6 +47,28 @@ class Admin extends CI_Controller {
 
 		$this->load->view('admin/dashboard', $data);
 	}
+	public function backup_database(){
+		$this->load->dbutil();
+
+		$prefs = array(     
+			'format'      => 'zip',             
+			'filename'    => 'erigrafika.sql',
+		);
+
+
+		$backup =& $this->dbutil->backup($prefs); 
+
+		$db_name = 'backup-on-'. date("Y-m-d-H-i-s") .'.zip';
+		$save = 'backup/'.$db_name;
+
+		$this->load->helper('file');
+		write_file($save, $backup); 
+
+
+		$this->load->helper('download');
+		force_download($db_name, $backup);
+		return 0;
+	}
 	public function layanan(){
 		$this->_make_sure_is_admin();
 
